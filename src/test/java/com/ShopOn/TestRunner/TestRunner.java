5e4d3fa2bfca1801fb0756dc3e77d324465ef513
Mahima <mahima.malik@amadeus.com>
2020-08-31 17:57:38 +0530

    initial commit

---
+++ src/test/java/com/ShopOn/TestRunner/TestRunner.java
@@ -0,0 +1,30 @@
+package com.ShopOn.TestRunner;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import com.ShopOn.Utilities.Email;
+import com.ShopOn.test.LoginTest;
+
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// TODO Auto-generated method stub
+		Result result=JUnitCore.runClasses(LoginTest.class);
+		System.out.println(result.getRunCount());
+		System.out.println(result.getFailureCount());
+		System.out.println(result.getRunTime());
+		//Email.sendEmail();
+		//MakePDF pdf=new MakePDF(result.getRunCount(),result.getFailureCount(),result.getRunTime());
+		//pdf.pdfMake();
+		//Email.sendEmail();
+		
+
+	}
+
+	}
+
+
---
+++ src/test/java/com/ShopOn/test/LoginTest.java
@@ -0,0 +1,61 @@
+package com.ShopOn.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.ShopOn.BaseTest.BaseTest;
+import com.ShopOn.PageObjects.LoginPage;
+import com.relevantcodes.extentreports.LogStatus;
+
+
+
+
+public class LoginTest extends BaseTest {
+	
+	@BeforeClass
+	public static void startTest() {
+		BaseTest.startTest();
+		test = report.startTest("Valid Details TestCase");
+	}
+	
+	public void setUp() {
+
+		super.setUp();
+	
+	}
+	
+	
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	
+	public static void endTest() {
+		BaseTest.endTest();
+	}
+	
+	
+	@Test
+	public void Login() throws InterruptedException {
+		
+		LoginPage objlogin = new LoginPage(super.driver);
+
+		objlogin.setUserName("qwerty1");
+
+		objlogin.setPassword("qwerty");
+		objlogin.clickSubmit();
+		Thread.sleep(1000);
+		assertTrue((driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/ul/li[4]/a")).isDisplayed()));
+		test.log(LogStatus.PASS, "Test Passed");
+		System.out.println("test passed");
+		
+		
+	}
+	
+	
+
+}
